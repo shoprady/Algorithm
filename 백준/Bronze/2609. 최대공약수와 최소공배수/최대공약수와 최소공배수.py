38346cadcc0337dd2f1e13560934180e3894c368
Seoyeon Ye <121270700+shoprady@users.noreply.github.com>
2024-07-08 20:38:35 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,29 +1,14 @@
 a, b = map(int, input().split())
 
-# a < b이도록 정렬
-if b < a:
-    temp = a
-    a = b
-    b = temp
+# 최대공약수
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
 
-# b가 a로 나누어떨어질 경우
-if b % a == 0:
-    x, y = a, b
-    
-else:
-    # 최대공약수
-    for i in range(1, a):
-        if a % i == 0:
-            if b % i == 0:
-                x = i
-    
-    # 최소공배수
-    order = 2
-    while True:
-        y = a * order
-        if y % b == 0:
-            break
-        order += 1
-    
-print(x)
-print(y)
+# 최소공배수
+def lcm(a, b):
+    return int(a * b / gcd(a, b))
+
+print(gcd(a, b))
+print(lcm(a, b))
