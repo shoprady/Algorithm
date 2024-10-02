2404f37c52d7a777cd5c7ec3b6459e932e2a96b5
Seoyeon Ye <121270700+shoprady@users.noreply.github.com>
2024-10-02 10:23:45 +0900

    [Silver III] Title: 회전하는 큐, Time: 64 ms, Memory: 34160 KB -BaekjoonHub

---
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+n, m = map(int, input().split())
+delete_nums = list(map(int, input().split()))
+nums = deque([i for i in range(1, n + 1)])
+
+count = 0
+for num in delete_nums:
+    if nums[0] == num:
+        nums.popleft()
+        n -= 1
+        continue
+
+    if nums.index(num) > n/2:
+        move = n - nums.index(num)
+    else:
+        move = - nums.index(num)
+    nums.rotate(move); nums.popleft()
+    count += abs(move)
+    n -= 1
+        
+print(count)
