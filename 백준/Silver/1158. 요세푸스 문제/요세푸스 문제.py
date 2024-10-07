8120cd5398f1f547c7bee61da6c5953b0635596d
Seoyeon Ye <121270700+shoprady@users.noreply.github.com>
2024-10-07 20:31:40 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 52 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -3,15 +3,8 @@ from collections import deque
 n, k = map(int, input().split())
 queue = deque(range(1, n + 1))
 
-nums = []
-while queue:
-    queue.rotate(-k+1)
-    nums.append(queue.popleft())
-
 print('<', end='')
-for i in range(len(nums)):
-    if i < len(nums) - 1:
-        print(nums[i], end=', ')
-    else:
-        print(nums[i], end='')
-print('>')
+while len(queue) > 1:
+    queue.rotate(-k+1)
+    print(queue.popleft(), end=', ')
+print(queue.popleft(), end='>')
