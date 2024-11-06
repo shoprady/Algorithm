63514bdcfb239a88c72b3a5ea31162893cd6d69c
Seoyeon Ye <121270700+shoprady@users.noreply.github.com>
2024-11-06 11:34:41 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 988 ms, Memory: 335260 KB -BaekjoonHub

--- 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.py
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.py
@@ -1,13 +1,13 @@
-def merge_sort(A, left, right):
-    if left < right:
-        mid = (left + right) // 2
-        merge_sort(A, left, mid)
-        merge_sort(A, mid + 1, right)
-        merge(A, left, mid, right)
+def merge_sort(A, p, r):
+    if p < r:
+        q = (p + r) // 2
+        merge_sort(A, p, q)
+        merge_sort(A, q + 1, r)
+        merge(A, p, q, r)
 
-def merge(A, left, mid, right):
-    i, j, tmp = left, mid + 1, []
-    while i <= mid and j <= right:
+def merge(A, p, q, r):
+    i, j, tmp = p, q + 1, []
+    while i <= q and j <= r:
         if A[i] <= A[j]:
             tmp.append(A[i])
             i += 1
@@ -15,14 +15,14 @@ def merge(A, left, mid, right):
             tmp.append(A[j])
             j += 1
             
-    while i <= mid: # 왼쪽 배열 남음
+    while i <= q: # 왼쪽 배열 남음
         tmp.append(A[i])
         i += 1
-    while j <= right: # 오른쪽 배열 남음
+    while j <= r: # 오른쪽 배열 남음
         tmp.append(A[j])
         j += 1
         
-    i = left
+    i = p
     for t in tmp:
         A[i] = t
         ans.append(t)
