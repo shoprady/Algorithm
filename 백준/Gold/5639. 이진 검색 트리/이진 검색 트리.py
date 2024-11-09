aa930b920fe8ffac2277e222e2ed4db2b94bff03
Seoyeon Ye <121270700+shoprady@users.noreply.github.com>
2024-11-09 19:32:58 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 240 ms, Memory: 143656 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -1,44 +1,21 @@
 import sys
 sys.setrecursionlimit(int(2e4))
 
-class TreeNode:
-    def __init__(self, item):
-        self.item = item
-        self.left = None
-        self.right = None
-        self.parent = None ###
-        
-def postorder(root):
-    if root is not None:
-        postorder(root.left)
-        postorder(root.right)
-        print(root.item)
-        
-tree = []
+def postorder(left, right):
+    if left > right:
+        return
+    mid = right + 1
+    for i in range(left + 1, right + 1):
+        if pre[i] > pre[left]:
+            mid = i; break
+    postorder(left + 1, mid - 1)
+    postorder(mid, right)
+    print(pre[left])
+
+pre = []
 while True:
     try:
-        tree.append(int(input()))
+        pre.append(int(input()))
     except: break
         
-root = TreeNode(tree[0])
-root.parent = None
-curr = root
-
-for i in range(1, len(tree)):
-    node = TreeNode(tree[i])
-    if tree[i] < curr.item:
-        curr.left = node
-        node.parent = curr
-        curr = node
-    else: # tree[i] >= curr.item
-        while curr != root:
-            if tree[i] > curr.parent.item:
-                curr = curr.parent
-            else: break
-        while curr.right:
-            curr = curr.right
-        curr.right = node
-        node.parent = curr
-        curr = node
-        
-postorder(root)
+postorder(0, len(pre) - 1)
