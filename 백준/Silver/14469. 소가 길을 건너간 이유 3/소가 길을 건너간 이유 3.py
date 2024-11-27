3a30d55fc8a69a7d5e24ab8d560113f932ee3fe2
Seoyeon Ye <121270700+shoprady@users.noreply.github.com>
2024-11-27 10:52:08 +0900

    [Silver IV] Title: 소가 길을 건너간 이유 3, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.py
+++ 백준/Silver/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.py
@@ -6,12 +6,7 @@ for _ in range(n):
 cows.sort()
 time = 0
 for i in range(len(cows)):
-    if time == 0:
-        time += cows[i][0] + cows[i][1]
-    elif time > cows[i][0]:
-        time += cows[i][1]
-    else:
-        time = cows[i][0]
-        time += cows[i][1]
+    time = max(time, cows[i][0])
+    time += cows[i][1]
         
 print(time)
