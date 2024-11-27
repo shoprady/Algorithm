d6956cef8d19f8680f218130c00045fd90cebc21
Seoyeon Ye <121270700+shoprady@users.noreply.github.com>
2024-11-27 10:44:37 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.py
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.py
@@ -3,16 +3,10 @@ levels = []
 for _ in range(n):
     levels.append(int(input()))
     
-def check_levels(levels):
-    ans = 0
-    for i in range(len(levels) - 1):
-        if levels[i] >= levels[i+1]:
-            diff = levels[i] - levels[i+1] + 1
-            levels[i] -= diff
-            ans += diff
-    return ans
-    
 ans = 0
-for _ in range(len(levels) - 1):
-    ans += check_levels(levels)
+for i in range(len(levels) - 1, 0, -1):
+    if levels[i] <= levels[i-1]:
+        diff = levels[i-1] - levels[i] + 1
+        levels[i-1] -= diff
+        ans += diff
 print(ans)
