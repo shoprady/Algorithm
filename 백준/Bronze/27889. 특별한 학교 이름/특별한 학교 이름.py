ca562d05bfecf9b327cb138d3241c30d053e50b2
Seoyeon Ye <121270700+shoprady@users.noreply.github.com>
2025-03-02 10:49:43 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,5 @@
+s = input()
+if s == 'NLCS': print('North London Collegiate School')
+elif s == 'BHA': print('Branksome Hall Asia')
+elif s == 'KIS': print('Korea International School')
+else: print('St. Johnsbury Academy')
