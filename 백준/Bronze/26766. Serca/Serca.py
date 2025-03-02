7c67e089ff91f09fb68900013c54a31aca0295a7
Seoyeon Ye <121270700+shoprady@users.noreply.github.com>
2025-03-03 08:01:21 +0900

    [Bronze V] Title: Serca, Time: 248 ms, Memory: 111636 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.py
@@ -0,0 +1,12 @@
+def heart():
+    print(' @@@   @@@ ')
+    print('@   @ @   @')
+    print('@    @    @')
+    print('@         @')
+    print(' @       @ ')
+    print('  @     @  ')
+    print('   @   @   ')
+    print('    @ @    ')
+    print('     @     ')
+for _ in range(int(input())):
+    heart()
