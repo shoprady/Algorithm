aec651b0cdc27fb98e2f3955a02415b5a97f9863
Seoyeon Ye <121270700+shoprady@users.noreply.github.com>
2025-03-02 11:05:49 +0900

    [Bronze V] Title: 2033년 밈 투표, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/29731. 2033년 밈 투표/2033년 밈 투표.py
@@ -0,0 +1,10 @@
+rick = {'Never gonna give you up': 0,
+        'Never gonna let you down': 0,
+        'Never gonna run around and desert you': 0,
+        'Never gonna make you cry': 0,
+        'Never gonna say goodbye': 0,
+        'Never gonna tell a lie and hurt you': 0,
+        'Never gonna stop': 0}
+for _ in range(int(input())):
+    if input() not in rick: print('Yes'); exit(0)
+print('No')
