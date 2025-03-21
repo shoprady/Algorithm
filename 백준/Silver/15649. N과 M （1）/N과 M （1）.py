1b8605a162d380fac35b95f28f351482795fa266
Seoyeon Ye <121270700+shoprady@users.noreply.github.com>
2025-03-21 22:10:32 +0900

    [Silver III] Title: N과 M (1), Time: 152 ms, Memory: 111488 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+ans = []
+def dfs():
+    if len(ans) == m:
+        print(' '.join(map(str, ans)))
+        return
+    for i in range(1, n + 1):
+        if i not in ans:
+            ans.append(i)
+            dfs()
+            ans.pop()
+dfs()
