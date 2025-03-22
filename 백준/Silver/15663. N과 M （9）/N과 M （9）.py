98b48f00a784b87e8d3c848217f8f217bfbf1abb
Seoyeon Ye <121270700+shoprady@users.noreply.github.com>
2025-03-22 13:19:52 +0900

    [Silver II] Title: N과 M (9), Time: 164 ms, Memory: 113420 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,10 +1,10 @@
 n, m = map(int, input().split())
-ls, ans, result = sorted(list(map(int, input().split()))), [], []
+ls, ans, result = sorted(list(map(int, input().split()))), [], set()
 def dfs():
     if len(ans) == m:
         tmp = ' '.join(map(lambda x: str(ls[x]), ans))
         if tmp not in result:
-            print(tmp); result.append(tmp)
+            print(tmp); result.add(tmp)
         return
     for i in range(n):
         if i not in ans:
