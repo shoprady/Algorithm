66aa0be7887f5bc501e917f30e2a3e2e9d55a98f
Seoyeon Ye <121270700+shoprady@users.noreply.github.com>
2025-03-24 16:03:04 +0900

    [Silver IV] Title: 별 찍기 - 19, Time: 180 ms, Memory: 113732 KB -BaekjoonHub

--- 백준/Silver/10994. 별 찍기 － 19/별 찍기 － 19.py
+++ 백준/Silver/10994. 별 찍기 － 19/별 찍기 － 19.py
@@ -1,17 +1,15 @@
-n = int(input())
-k = 4 * n - 3
-stars = [[' '] * k for _ in range(k)]
+n = 4 * int(input()) - 3
+stars = [[' '] * n for _ in range(n)]
 def star(n, x, y):
     if n == 1:
         stars[x][y] = '*'; return
-    k = 4 * n - 3
-    for i in range(x, x + k):
-        for j in range(y, y + k):
-            if i == x or i == x + k - 1 or j == y or j == y + k - 1:
+    for i in range(x, x + n):
+        for j in range(y, y + n):
+            if i == x or i == x + n - 1 or j == y or j == y + n - 1:
                 stars[i][j] = '*'
-    star(n - 1, x + 2, y + 2)
+    star(n - 4, x + 2, y + 2)
 star(n, 0, 0)
-for i in range(k):
-    for j in range(k):
+for i in range(n):
+    for j in range(n):
         print(stars[i][j], end='')
     print('')
