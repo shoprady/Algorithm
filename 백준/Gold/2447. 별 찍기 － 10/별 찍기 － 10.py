5234333f60b631dfbdf69b23429ea8655fa105b0
Seoyeon Ye <121270700+shoprady@users.noreply.github.com>
2025-03-25 15:01:50 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 528 ms, Memory: 161180 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -0,0 +1,20 @@
+def square(n, x, y):
+    if n == 3:
+        for i in range(x, x + 3):
+            for j in range(y, y + 3):
+                if i == x + 1 and j == y + 1: continue
+                board[i][j] = '*'
+        return
+    k = n // 3
+    for i in range(3):
+        for j in range(3):
+            if i == 1 and j == 1: continue
+            square(k, x + k * i, y + k * j)
+
+n = int(input())
+board = [[' '] * n for _ in range(n)]
+square(n, 0, 0)
+for i in range(n):
+    for j in range(n):
+        print(board[i][j], end='')
+    print('')
