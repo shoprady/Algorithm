438100ad1f6a7f9c7157ec40589d0cf5c3a5fbc8
Seoyeon Ye <121270700+shoprady@users.noreply.github.com>
2025-03-31 15:17:20 +0900

    [Silver I] Title: IOIOI, Time: 224 ms, Memory: 211720 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,13 +1,24 @@
 n = int(input()) * 2 + 1
 length = int(input())
-s = input()
+s = list(input())
 
-p = ''
-for i in range(n):
-    if i % 2 == 0: p += 'I'
-    else: p += 'O'
+total, I_check, ls = 0, False, []
+for i in range(length):
+    if s[i] == 'I' and not I_check:
+        I_check = True
+        total += 1
+        ls.append(total)
+    elif s[i] == 'O' and I_check:
+        I_check = False
+        total += 1
+        ls.append(total)
+    else:
+        if s[i] == 'I': total = 1
+        else: total = 0
+        ls.append(total)
 
 ans = 0
-for i in range(length - n + 1):
-    if s[i:i+n] == p: ans += 1
+for i in ls:
+    if i < n: continue
+    elif i >= n and i % 2 == 1: ans += 1
 print(ans)
