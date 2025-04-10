c6883731f13570fcf4b739e26757118e727989f0
Seoyeon Ye <121270700+shoprady@users.noreply.github.com>
2025-04-10 12:38:12 +0900

    [Bronze III] Title: 세 수, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/2985. 세 수/세 수.py
@@ -0,0 +1,17 @@
+a, b, c = map(int, input().split())
+if a + b == c:
+    print('{0}+{1}={2}'.format(a, b, c))
+elif a - b == c:
+    print('{0}-{1}={2}'.format(a, b, c))
+elif a * b == c:
+    print('{0}*{1}={2}'.format(a, b, c))
+elif a // b == c:
+    print('{0}/{1}={2}'.format(a, b, c))
+elif a == b + c:
+    print('{0}={1}+{2}'.format(a, b, c))
+elif a == b - c:
+    print('{0}={1}-{2}'.format(a, b, c))
+elif a == b * c:
+    print('{0}={1}*{2}'.format(a, b, c))
+else:
+    print('{0}={1}/{2}'.format(a, b, c))
